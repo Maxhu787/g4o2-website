9d53a51f02c901582f64faaabda8f63e728c31e0
maxhu787 <maxhu787@gmail.com>
2022-07-04 21:26:24 +0800

    dd

--- autos/index.php
+++ autos/index.php
@@ -14,8 +14,11 @@
         </p>
         <p>
             Attempt to go to <a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
-            <!--<p>
+        </p>    <!--<p>
             <a href="http://www.wa4e.com/code/rps.zip" target="_blank">Source Code for this Application</a>
         </p>-->
+        <p>
+            Go back to <a href="../index.html">main</a> page
+        </p>
     </div>
 </body>
--- pdo/pdo.php
+++ pdo/pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'g4o2', 'g4o2');;
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'g4o2', 'g4o2/');;
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
